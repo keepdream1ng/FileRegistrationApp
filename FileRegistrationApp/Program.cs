736306e3a6a4ce83a9bab5fb2ef584dba505e2f0
Anatolii Ivanov <gotnickname@gmail.com>
2025-09-09 22:01:28 +0400

    Add project files.

---
+++ FileRegistrationApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using FileRegistrationApp.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace FileRegistrationApp;
+
+internal class Program
+{
+	static async Task Main(string[] args)
+	{
+		ConfigureLogging();
+		try
+		{
+			Log.Information("Starting application...");
+
+			var builder = new HostBuilder()
+				.ConfigureServices((hostContext, services) =>
+				{
+					services.AddSingleton<FileWatherServiceConfig>(GetWatcherConfig(args));
+					services.AddHostedService<FileWatcherService>();
+				})
+				.UseSerilog()
+				.UseConsoleLifetime();
+
+			await builder.Build().RunAsync();
+		}
+		catch (Exception ex)
+		{
+			Log.Fatal(ex, "Application terminated unexpectedly");
+		}
+		finally
+		{
+			Log.CloseAndFlush();
+		}
+	}
+
+	private static void ConfigureLogging()
+	{
+		Log.Logger = new LoggerConfiguration()
+			.MinimumLevel.Information()
+			.Enrich.FromLogContext()
+			.WriteTo.Console(
+				 outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+			.WriteTo.File(
+				path: "logs/app-.log",
+				rollingInterval: RollingInterval.Day,
+				retainedFileCountLimit: 7,
+				outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+				shared: true)
+			.CreateLogger();
+	}
+
+	private static FileWatherServiceConfig GetWatcherConfig(string[] args)
+	{
+		var config = new FileWatherServiceConfig();
+		if (args.Length == 0)
+		{
+			config.Path = ConfigurationManager.AppSettings["RegisterDirectoryPath"];
+		}
+		else
+		{
+			config.Path = args[0];
+		}
+		return config;
+	}
+}
---
+++ FileRegistrationApp/Services/FileWatcherService.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FileRegistrationApp.Services;
+public class FileWatcherService : IHostedService
+{
+	private FileSystemWatcher _watcher;
+	private readonly FileWatherServiceConfig _config;
+	private readonly ILogger<FileWatcherService> _logger;
+
+	public FileWatcherService(
+		FileWatherServiceConfig config,
+		ILogger<FileWatcherService>  logger
+		)
+    {
+		_config = config;
+		_logger = logger;
+	}
+
+    public Task StartAsync(CancellationToken cancellationToken)
+	{
+		StartFileWatcher();
+		return Task.CompletedTask;
+	}
+
+	public Task StopAsync(CancellationToken cancellationToken)
+	{
+		StopFileWatcher();
+		return Task.CompletedTask;
+	}
+
+	private void StartFileWatcher()
+	{
+		_watcher = new FileSystemWatcher();
+		_watcher.Path = _config.Path;
+		_watcher.Created += NotifyAboutFile;
+		_watcher.EnableRaisingEvents = true;
+		_logger.LogInformation($"Wathing path: {_config.Path}");
+	}
+
+	private void StopFileWatcher()
+	{
+		if (_watcher is not null)
+		{
+			_watcher.Created -= NotifyAboutFile;
+			_watcher.Dispose();
+			_watcher = null;
+			_logger.LogInformation("Watching stopped");
+		}
+	}
+
+	private void NotifyAboutFile(object sender, FileSystemEventArgs e)
+	{
+		_logger.LogInformation($"New file added {e.Name}");
+	}
+}
+
+public class FileWatherServiceConfig
+{
+	public string Path { get; set; }
+}
